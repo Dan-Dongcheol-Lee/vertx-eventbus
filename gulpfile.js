ec67016a829b9bde9b487ae484da4eb080fc7034
Dan-Dongcheol-Lee <leedc123@gmail.com>
2015-01-07 00:26:16 +1100

    Fixed minors

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 var gulp = require('gulp');
-var runSequence = require('run-sequence');
-var mocha = require('gulp-mocha');
 require('web-component-tester').gulp.init(gulp);
 
 gulp.task('default', ['test']);
